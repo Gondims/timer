cd70f60e3f946f839b50855ab27a375048511c5f
Gondims <leticiagondim1.0@gmail.com>
2024-05-13 21:11:49 -0400

    Contexts

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from "./styles/global"
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from "./Router"
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 function App() {
 
@@ -10,7 +11,9 @@ function App() {
     <>
       <ThemeProvider theme={defaultTheme}>
         <BrowserRouter>
+        <CyclesContextProvider>
           <Router />
+        </CyclesContextProvider>
         </BrowserRouter>
         <GlobalStyle />
       </ThemeProvider>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,14 @@
 import { HeaderContainer } from './styles'
 import { Scroll, Timer } from 'phosphor-react'
 
-
+import logoGondim from '../../assets/gondim_logo.svg'
 import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
       <span>
-        {/* <img src={logoIgnite} alt="" /> */}
+        <img src={logoGondim} alt="" />
       </span>
       <nav>
         <NavLink to="/" title="Timer">
